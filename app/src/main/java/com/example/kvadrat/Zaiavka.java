ae2f88c31ae22205266eba45ff534a6a10fce1dd
kryska1709 <157532841+kryska1709@users.noreply.github.com>
2024-10-25 01:01:57 +0300

    toast

--- app/src/main/java/com/example/kvadrat/Zaiavka.java
+++ app/src/main/java/com/example/kvadrat/Zaiavka.java
@@ -1,23 +1,4 @@
 package com.example.kvadrat;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Zaiavka {
     private String message;
     private String userEmail;
